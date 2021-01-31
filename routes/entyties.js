2b69ab56353c5f98e538ec59e2c4a70cd283c4b3
YVitalii <0504238839@ukr.net>
2021-01-31 23:25:55 +0200

    Путь entyties - работает[F

--- routes/entyties.js
+++ routes/entyties.js
@@ -1,21 +1,24 @@
 var express = require('express');
 var router = express.Router();
-var entyties=require('../config.js').entyties;
+var listEntyties=require('../config.js').entyties;
 // ------------ логгер  --------------------
 const l = require('../tools/log.js'); // логер
 let logName="<"+(__filename.replace(__dirname,"")).slice(1)+">:";
 let gTrace=1; //=1 глобальная трассировка (трассируется все)
 
-
-/* GET /entyties  выдает список печей с их характеристиками */
+gTrace ?  l('i',logName) : null;
+// /* GET /entyties  выдает список печей с их характеристиками */
+// router.get('/', function(req, res, next) {
+//   // -- настройки логгера --------------
+//   let trace=1;
+//   let logN=logName+"POST:/entyties => ";trace = ((gTrace !== 0) ? gTrace : trace);
+//   //-------------------------------------
+//   trace ?  l('i',logN,"req=",req) : null;
+//   let data
+//   res.status(200).send("Ok");
+// });
 router.post('/', function(req, res, next) {
-  // -- настройки логгера --------------
-  let trace=1;
-  let logN=logName+"POST:/entyties => ";trace = ((gTrace !== 0) ? gTrace : trace);
-  //-------------------------------------
-  trace ?  l('i',logN,"req=",req) : null;
-  let data
-  res.status(200).send("Ok");
+  res.status(200).json(listEntyties);//.send('respond with a resource');
 });
 
 module.exports = router;
