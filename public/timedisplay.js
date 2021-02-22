a566f4f90d32d6c54697a054f29e157e2603af5f
DKuziuk <neyden@ukr.net>
2021-02-22 23:02:22 +0200

    минус баги даты

--- public/timedisplay.js
+++ public/timedisplay.js
@@ -1,26 +1,6 @@
 function getDate()
 {
   var date = new Date();
-  var month = date.getMonth()+1;
-  var day = date.getDay();
-  var hours = date.getHours();
-  var minutes = date.getMinutes();
-  var seconds = date.getSeconds();
-  if(month < 10){
-    month = '0' + month;
-  }
-  if(day < 10){
-    day = '0' + day;
-  }
-  if(seconds < 10){
-    seconds = '0' + seconds;
-  }
-  if(minutes < 10){
-    minutes = '0' + minutes;
-  }
-  if(hours < 10){
-    hours = '0' + hours;
-  }
-  document.getElementById('timedisplay').innerHTML = month + "." + day + " " + hours + ':' + minutes + ':' + seconds;
+  document.getElementById('timedisplay').innerHTML = date.toLocaleString();
 }
 setInterval(getDate, 0);
