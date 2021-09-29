51d2017f37b6299cd7d5815512dffc04fc6080ee
YVitalii <0504238839@ukr.net>
2021-09-29 22:03:23 +0300

    2021-09-29 homework

--- processes/thermProcess/_test.js
+++ processes/thermProcess/_test.js
@@ -1,3 +1,11 @@
-const ThermProcess = require("../../processes/ThermProcess/thermStep.js");
-let thermProcess = new ThermProcess();
-thermProcess.addStep({ holdTime: 50 });
+const ThermStep = require("../../processes/ThermProcess/thermStep.js");
+// настройки шага
+let task = {
+  taskT: 1120,
+  heatTime: 4 * 60,
+  holdTime: 60,
+  timeErr: 30,
+};
+
+let thermProcess = new ThermStep(task);
+//thermProcess.addStep({ holdTime: 50 });
