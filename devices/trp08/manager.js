6934e24530b5c560cabd3454201b9eeb17cda247
YVitalii <0504238839@ukr.net>
2023-04-20 11:01:52 +0300

    2023-04-18

--- devices/trp08/manager.js
+++ devices/trp08/manager.js
@@ -1,12 +1,15 @@
+const { forEach } = require("core-js/core/array");
 const device = require("./driver.js");
 
 class Manager {
   constructor(iface, id) {
+    this.trace = 1;
+    this.ln = `managerTRP08(${id}):`;
     this.iface = iface;
     this.id = id;
     this.device = device;
   }
-  async setParams() {}
+  async setParams(params = {}) {}
   async start() {}
   async stop() {}
   async getT() {}
