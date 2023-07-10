1847881c8067a4212ec504dfa289ac54e18a418f
dkuziuk <neyden@ukr.net>
2023-07-10 14:49:42 +0300

    modified:   conf_iface.js

--- conf_iface.js
+++ conf_iface.js
@@ -3,7 +3,7 @@ let platform = process.platform;
 if (platform != "win32") {
   comName = "/dev/ttyUSB0";
 } else {
-  comName = "COM3";
+  comName = "COM4";
 }
 // параметры последовательного порта
 const connection = {
