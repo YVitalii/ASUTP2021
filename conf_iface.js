346f78b841e15012d7ba0edf1d75a9a08aa6a800
dkuziuk <neyden@ukr.net>
2023-08-23 17:21:46 +0300

    Запис/читання аналогових значень в відсотках

--- conf_iface.js
+++ conf_iface.js
@@ -3,14 +3,14 @@ let platform = process.platform;
 if (platform != "win32") {
   comName = "/dev/ttyUSB0";
 } else {
-  comName = "COM6";
+  comName = "COM5";
 }
 // параметры последовательного порта
 const connection = {
   path: comName, //путь к последовательному порту
   openOptions: {
     // параметры порта
-    baudRate: 9600, // скорость бод
+    baudRate: 2400, // скорость бод
   },
   timeoutBetweenCalls: 300, // пауза между запросами, мс т.к. ТРП-08 тупят, то бывает начинают отвечать после timeout и начинают сбивать все передачи
 }; // connection
